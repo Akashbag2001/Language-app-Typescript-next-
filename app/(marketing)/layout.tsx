f1f668ea3f8e64c49bd8c656c8f72fa7aa0bee4d
Akashbag2001 <bagakash11@gmail.com>
2024-07-24 19:22:44 +0530

    Second commit

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,20 @@
+import {Header} from "./header";
+
+
+type Props = {
+    children: React.ReactNode;
+};
+
+const MarketingLayout =  ({children}: Props)=>{
+    return(
+        
+        <div className="min-h-screen flex flex-col">
+            <Header/>
+            <main className="flex-1 flex flex-col justify-center items-center">
+            {children}
+            </main>
+        </div>
+    )
+}
+
+export default MarketingLayout;
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center">
+     
+
+      <h1>This a marketing page!!</h1>
+    </div>
+  );
+}
